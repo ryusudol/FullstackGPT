4a7da8f5c4084d1ae17f9070745dcc4619b18940
Suhyeon <fbtngus2789@naver.com>
2024-01-02 20:42:31 +0900

    DocumentGPT finished

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,15 +1,23 @@
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.callbacks.base import BaseCallbackHandler
 
-st.set_page_config(
-    page_title="DocumentGPT",
-    page_icon="📜",
-)
 
+st.set_page_config(page_title="DocumentGPT", page_icon="📜")
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+@st.cache_data(show_spinner="Embedding file . . .")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -20,9 +28,8 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/1984.txt")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-
     embeddings = OpenAIEmbeddings()
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
@@ -30,20 +37,93 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context.
+            If you don't know the answer, just say you don't know.
+            DON'T make anything up.
+            
+            Context: {context}
+            """
+        ),
+        (
+            "human",
+            "{question}"
+        ),
+    ]
+)
+
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+    
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+    
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+    
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[ChatCallbackHandler()]
+)
+
+
 st.title("DocumentGPT")
 
 st.markdown("""
     Welcome!
-    
+
     Use the chatbot to ask questions to an AI about your files!
+    
+    Upload your files on the sidebar.
 """)
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"]
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"]
+    )
 
 if file:
     retriever = embed_file(file)
-    docs = retriever.invoke("winston")
-    st.write(docs)
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your documents!")
+    if message:
+        send_message(message, "human")
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough()
+        } | prompt | llm
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+else:
+    st.session_state["messages"] = []
+    
